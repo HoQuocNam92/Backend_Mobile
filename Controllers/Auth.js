4c88c74fbec87afb3425ce9a8c178d1515e8f723
hoquocnam92 <hoquocnam92@gmail.com>
2024-11-16 11:52:36 +0700

    backend

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,22 +1,21 @@
 const User = require('../server/createUser');
-const Product = require("../server/createProduct");
-const jwt = require('jsonwebtoken')
+const Product = require('../server/createProduct');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
 dotenv.config();
 const Authentication = {
     register: async (req, res) => {
         const { username, password, email } = req.body;
-        console.log("Check Backend ", username, password, email);
+        console.log('Check Backend ', username, password, email);
         try {
             const salt = await bcrypt.genSalt(10);
             const hashed = await bcrypt.hash(password, salt);
             const user = await new User({
-
                 username: username,
                 email: email,
                 password: hashed,
-            })
+            });
             const Savenew = await user.save();
             return res.status(200).json(Savenew);
         } catch (err) {
@@ -25,54 +24,47 @@ const Authentication = {
     },
     login: async (req, res) => {
         const { username, password } = req.body;
-        console.log("Check data backend login ", username, password);
+        console.log('Check data backend login ', username, password);
         try {
             const Users = await User.findOne({ username });
             if (!Users) {
-                return res.status(404).json("Wrong user");
+                return res.status(404).json('Wrong user');
             }
-            const passwordCheck = await bcrypt.compare(
-                password,
-                Users.password,
-
-            );
+            const passwordCheck = await bcrypt.compare(password, Users.password);
             if (!passwordCheck) {
-                return res.status(404).json("Wrong password");
+                return res.status(404).json('Wrong password');
             }
             if (Users && passwordCheck) {
-
-
-
-                const token = jwt.sign({ Users: Users }, 'your_jwt_secret', { expiresIn: '1h' });
+                const token = jwt.sign({ Users: Users }, 'your_jwt_secret', {
+                    expiresIn: '1h',
+                });
                 return res.json({ token });
             }
         } catch (err) {
-            res.status(500).json("Ket noi bi loi roi !!");
+            res.status(500).json('Ket noi bi loi roi !!');
         }
-
     },
     Cart: async (req, res) => {
-        const { id, name, price, oldPrice } = req.body;
-        console.log("Data nhan dc", id, name, price, oldPrice)
+        const { id, name, price, oldPrice, quantity } = req.body;
+        console.log('Data nhan dc', id, name, price, oldPrice);
         try {
             const product = await new Product({
                 _id: id,
                 name: name,
                 price: price,
-                oldPrice: oldPrice
-            })
+                oldPrice: oldPrice,
+                quantity: quantity,
+            });
             const SaveProduct = await product.save();
             res.status(200).json(SaveProduct);
-
-        }
-        catch (err) {
+        } catch (err) {
             res.status(500).json(err);
         }
     },
     ProductCart: async (req, res) => {
         try {
             const products = await Product.find();
-            console.log("Check data truocs khi gui ve ", products)
+            console.log('Check data truocs khi gui ve ', products);
             res.json(products);
         } catch (err) {
             res.status(500).json(err);
@@ -81,25 +73,22 @@ const Authentication = {
     loginDelete: async (req, res) => {
         try {
             const User = await user.findById(req.params.id);
-            res.status(200).json("Delet thanh cong")
-        }
-        catch (err) {
+            res.status(200).json('Delet thanh cong');
+        } catch (err) {
             res.status(500).json(err);
         }
     },
     removeCart: async (req, res) => {
         const id = req.params.id;
-        console.log(id);
-        console.log(typeof (id));
+        console.log('Check ID backend', id);
+        console.log(typeof id);
         try {
-            await Product.findByIdAndDelete(id)
+            await Product.findByIdAndDelete(id);
 
-            res.status(200).json("Xoa data thanh cong");
-
-        }
-        catch (err) {
+            res.status(200).json('Xoa data thanh cong');
+        } catch (err) {
             res.status(500).json(err);
         }
-    }
-}
+    },
+};
 module.exports = Authentication;
--- server/createProduct.js
+++ server/createProduct.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
-
     _id: {
         type: String,
         require: true,
     },
     name: {
         type: String,
-        required: true
+        required: true,
     },
     price: {
         type: String,
-        required: true
+        required: true,
     },
     oldPrice: {
         type: String,
-        required: true
-    }
-
-})
-const ModelProduct = mongoose.model("Product", ProductSchema);
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+});
+const ModelProduct = mongoose.model('Product', ProductSchema);
 module.exports = ModelProduct;
